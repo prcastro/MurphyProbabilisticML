5ccc43b7ca383eb5f6a592392acf9456d57dc703
Paulo R. O. Castro <brk00@fake.server>
2016-05-05 20:30:06 -0300

    Added some nice graphics to notebook and LinReg

---
+++ src/LinearRegression.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class LinearRegression():
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        self.W = np.linalg.pinv(X.T @ X) @ (X.T @ y) # the @ operator calls __matmul__
+
+    def _predict_point(self, x):
+        return self.W.dot(x)
+
+    def predict(self, x):
+        if len(x.shape) == 1:
+            return self._predict_point(x)
+        elif len(x.shape) == 2:
+            return np.array([self._predict_point(x_i) for x_i in x])
+        else:
+            raise ValueError
---
+++ src/PolynomialFeatures.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class PolynomialFeatures():
+    def __init__(self):
+        pass
+
+    def transform(self, X):
+        pass
