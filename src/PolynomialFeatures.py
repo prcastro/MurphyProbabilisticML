fb5b7bc3f46f72c2ba031fe108bf638e91a2bed3
prcastro <p.oliveira.castro@gmail.com>
2016-05-07 13:24:59 -0300

    Working polynomial features

--- src/PolynomialFeatures.py
+++ src/PolynomialFeatures.py
@@ -1,8 +1,24 @@
 import numpy as np
+import itertools
+
 
 class PolynomialFeatures():
     def __init__(self, n):
         self.n = n
 
     def fit_transform(self, X):
-        return np.hstack([X**i for i in range(1, self.n +1)])
+        if len(X.shape) > 2:
+            raise ValueError
+        result = X
+        last_dim = len(X.shape) - 1
+        number_features = X.shape[last_dim]
+        combinations = itertools.combinations_with_replacement(
+                                 range(number_features), self.n)
+        for combination in combinations:
+            if len(X.shape) == 1:
+                feature = X[combination, ]
+            else:
+                feature = X[:, combination]
+            feature = feature.prod(axis=last_dim, keepdims=True)
+            result = np.hstack((result, feature))
+        return result
