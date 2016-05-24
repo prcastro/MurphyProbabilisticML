8c7b4275bda0859836b53e237adb8d103a5b2af8
Paulo R. O. Castro <brk00@fake.server>
2016-05-24 18:53:55 -0300

    Bias on Linear Reg

--- src/LinearRegression.py
+++ src/LinearRegression.py
@@ -6,12 +6,16 @@ class LinearRegression():
         pass
 
     def fit(self, X, y):
+        bias = np.ones((X.shape[0], 1))
+        X = np.hstack((bias, X))
         self.W = np.linalg.pinv(X.T @ X) @ (X.T @ y)
 
     def _predict_point(self, x):
+        x = np.hstack(([1], x))
         return self.W.T @ x
 
     def predict(self, X):
+        
         if len(X.shape) == 1:
             return self._predict_point(X)
         elif len(X.shape) == 2:
